9d417a8032cbd4f965028c84d84f875a8f1a84eb
Paulus Esterhazy <pesterhazy@gmail.com>
2019-12-01 10:43:41 +0100

    WIP

--- src/advent/puzzle01.clj
+++ src/advent/puzzle01.clj
@@ -1,11 +1,17 @@
 (ns advent.puzzle01)
 
-(defn fuel [mass] (-> mass (quot 3) (- 2)))
+(defn fuel [mass] (-> mass (quot 3) (- 2) (Math/max 0)))
+
+(defn fuel* [mass]
+  (->> (iterate fuel mass) (drop 1) (take-while pos?) (apply +)))
 
 (def numbers
   (->> (clojure.java.io/reader "inputs/1.txt")
        line-seq
        (map #(Long/parseLong %))))
 
-(defn result []
+(defn solution []
   (->> (map fuel numbers) (apply +)))
+
+(defn solution-2 []
+  (->> (map fuel* numbers) (apply +)))
