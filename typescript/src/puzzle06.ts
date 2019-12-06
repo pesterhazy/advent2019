2c72b2f0be1d4e2092a14ad98c696fd076d2d9a1
Paulus Esterhazy <pesterhazy@gmail.com>
2019-12-06 08:37:25 +0100

    WIP

--- typescript/src/puzzle06.ts
+++ typescript/src/puzzle06.ts
@@ -12,13 +12,36 @@ const objectFromEntries = (x: any[]) =>
     return prev;
   }, {});
 
-const readInput = (): any => objectFromEntries(util.readCSV("6.txt", /\)/));
+const readInput = (): any =>
+  objectFromEntries(R.map(([a, b]) => [b, a], util.readCSV("6.txt", /\)/)));
 
 function solution() {
   let o = readInput();
+  let parents: { [key: string]: string[] } = {};
+  let count = 0;
   for (let k in o) {
-    console.log(k);
+    let orig = k;
+    parents[orig] = [];
+    while (o[k]) {
+      k = o[k];
+      count++;
+      parents[orig].push(k);
+    }
   }
+  console.log(count);
+
+  let src = ["YOU", ...parents[o["YOU"]]],
+    dst = ["SAN", ...parents[o["SAN"]]];
+
+  // console.log(src, dst);
+
+  let i, j;
+  for (i = 0; i < src.length; i++) {
+    j = dst.indexOf(src[i]);
+    if (j == -1) continue;
+    break;
+  }
+  console.log(i + (j as number));
 }
 
 export default solution;
