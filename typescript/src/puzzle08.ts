c42d64970329b3c92fff3bdc829d922c634b6454
Paulus Esterhazy <pesterhazy@gmail.com>
2019-12-08 10:30:52 +0100

    WIP

--- typescript/src/puzzle08.ts
+++ typescript/src/puzzle08.ts
@@ -33,25 +33,52 @@ const countChars = (needle: string, lines: Layer) =>
 function solution() {
   let layers = readInput();
 
-  let min = Number.POSITIVE_INFINITY;
-  let layer: Layer | undefined;
+  {
+    let min = Number.POSITIVE_INFINITY;
+    let layer: Layer | undefined;
 
-  for (let lines of layers) {
-    let v = countChars("0", lines);
+    for (let lines of layers) {
+      let v = countChars("0", lines);
 
-    if (v < min) {
-      min = v;
-      layer = lines;
+      if (v < min) {
+        min = v;
+        layer = lines;
+      }
     }
+
+    if (layer == undefined) throw new Error("Invariant violation");
+
+    console.log(layer);
+
+    let result = countChars("1", layer) * countChars("2", layer);
+
+    console.log(result);
   }
 
-  if (layer == undefined) throw new Error("Invariant violation");
+  {
+    for (let y = 0; y < HEIGHT; y++) {
+      let rline = "";
+      for (let x = 0; x < WIDTH; x++) {
+        let v;
 
-  console.log(layer);
+        for (let lines of layers) {
+          let vv = lines[y][x];
 
-  let result = countChars("1", layer) * countChars("2", layer);
+          if (vv === "0" || vv === "1") {
+            v = vv;
+            break;
+          }
+        }
+        let px;
+        if (v === "1") px = "\u2588";
+        else if (v === "0") px = " ";
+        else throw new Error("Invalid pixel");
 
-  console.log(result);
+        rline += px;
+      }
+      console.log(rline);
+    }
+  }
 }
 
 export default solution;
