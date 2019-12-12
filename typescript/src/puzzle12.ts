920c3067106cfc205a5d267a248ef2b8bf8f157d
Paulus Esterhazy <pesterhazy@gmail.com>
2019-12-13 00:13:34 +0100

    WIP

--- typescript/src/puzzle12.ts
+++ typescript/src/puzzle12.ts
@@ -7,8 +7,9 @@ interface Moon {
   vel: math.Matrix;
 }
 
+// let example = "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>";
 let example =
-  "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>";
+  "<x=1, y=-4, z=3>\n<x=-14, y=9, z=-4>\n<x=-4, y=-6, z=7>\n<x=6, y=-9, z=-11>";
 
 const readInput = (): Moon[] => {
   const lines = example.split(/\n/);
@@ -55,18 +56,67 @@ function applyVelocity(m: Moon) {
   m.pos = math.add(m.pos, m.vel) as math.Matrix;
 }
 
+function energy(m: Moon) {
+  let pot = R.reduce(
+    (a: number, b: number) => a + b,
+    0,
+    R.map(n => Math.abs(n), m.pos.valueOf() as number[])
+  );
+
+  let kin = R.reduce(
+    (a: number, b: number) => a + b,
+    0,
+    R.map(n => Math.abs(n), m.vel.valueOf() as number[])
+  );
+
+  return [pot, kin];
+}
+
+function ser(moons: Moon[]) {
+  let s = "";
+  for (let m of moons) {
+    s = s + JSON.stringify([m.pos.valueOf(), m.vel.valueOf()]);
+  }
+  return s;
+}
+
 function solution() {
   let moons: Moon[] = readInput();
-  for (let i = 0; i < 10; i++) {
-    console.log("step", i);
-    applyGravity(moons[0], [moons[1], moons[2], moons[3]]);
-    applyGravity(moons[1], [moons[0], moons[2], moons[3]]);
-    applyGravity(moons[2], [moons[0], moons[1], moons[3]]);
-    applyGravity(moons[3], [moons[0], moons[1], moons[2]]);
+  {
+    let total = 0;
     for (let moon of moons) {
       applyVelocity(moon);
-      console.log("pos", moon.pos.valueOf());
-      console.log("vel", moon.vel.valueOf());
+      let [pot, kin] = energy(moon);
+      let tot = pot * kin;
+      total += tot;
+    }
+    console.log("start", "\t", total);
+  }
+  {
+    let seen = new Set();
+    for (let i = 0; true; i++) {
+      applyGravity(moons[0], [moons[1], moons[2], moons[3]]);
+      applyGravity(moons[1], [moons[0], moons[2], moons[3]]);
+      applyGravity(moons[2], [moons[0], moons[1], moons[3]]);
+      applyGravity(moons[3], [moons[0], moons[1], moons[2]]);
+      let sumPot = 0;
+      let sumKin = 0;
+      let sumTot = 0;
+      for (let moon of moons) {
+        applyVelocity(moon);
+        let [pot, kin] = energy(moon);
+        let tot = pot * kin;
+        sumPot += pot;
+        sumKin += kin;
+        sumTot += tot;
+      }
+      if (0 === i % 1000) console.log(i);
+      if (seen.has(ser(moons))) {
+        console.log("FOUND", i, sumTot);
+        console.log(i, "\t", sumPot, "\t", sumKin, "\t", sumTot);
+        return;
+      }
+      seen.add(sumTot);
     }
   }
 }
