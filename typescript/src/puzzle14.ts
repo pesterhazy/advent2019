d809c1e967c4fbbd7656b8b07b1f3564ec3c00f9
Paulus Esterhazy <pesterhazy@gmail.com>
2019-12-14 16:50:47 +0100

    WIP

--- typescript/src/puzzle14.ts
+++ typescript/src/puzzle14.ts
@@ -6,7 +6,7 @@ let example =
   "9 ORE => 2 A\n8 ORE => 3 B\n7 ORE => 5 C\n3 A, 4 B => 1 AB\n5 B, 7 C => 1 BC\n4 C, 1 A => 1 CA\n2 AB, 3 BC, 4 CA => 1 FUEL";
 
 interface Recipe {
-  qty: string;
+  qty: number;
   ingredients: any[];
 }
 
@@ -18,16 +18,19 @@ const readInput = (): Record<string, Recipe> => {
       let matches = line.match(/(-?\d+|[A-Za-z]+)/g);
       if (!matches) throw new Error("Not found");
       let xs: string[][] = R.splitEvery(2, matches);
-      let pairs = R.init(xs);
+      let ingredients = R.map(
+        ([qty, mat]): any => ({ qty: parseInt(qty), mat: mat }),
+        R.init(xs)
+      );
       let [qty, mat] = R.last(xs) as [string, string];
-      return [mat, { qty: qty, ingredients: pairs }];
+      return [mat, { qty: parseInt(qty), ingredients: ingredients }];
     }, lines)
   );
 };
 
 function solution() {
   let input = readInput();
-  console.log(input["FUEL"]);
+  console.log(JSON.stringify(input, null, 4));
 }
 
 export default solution;
