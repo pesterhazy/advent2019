d7c6562474cdbdceffaaeff19fbb9c5b55f78335
Paulus Esterhazy <pesterhazy@gmail.com>
2020-01-01 13:38:25 +0100

    WIP

--- typescript/src/puzzle22.ts
+++ typescript/src/puzzle22.ts
@@ -16,7 +16,7 @@ interface Step {
 type Stack = number[];
 
 const readInput = (): Step[] =>
-  util.readLines("22.txt").map(l => {
+  util.readLines("22-1.txt").map(l => {
     let matches = l.match(/-?\d+/g);
     if (l.match(/deal into new stack/)) return { tag: Tag.New };
     if (l.match(/deal with increment/))
@@ -47,22 +47,44 @@ const next = (stack: Stack, { tag, v }: Step) => {
   throw "Unknown tag";
 };
 
-const N = 10007;
-// const N = 10;
+function mod(n: number, m: number) {
+  return ((n % m) + m) % m;
+}
+
+const calc = (i: number, { tag, v }: Step) => {
+  if (tag === Tag.New) {
+    return N - i;
+  }
+  if (tag === Tag.Cut) {
+    if (v == undefined) throw "Missing v";
+    return mod(i + v, N);
+  }
+  if (tag === Tag.Inc) {
+    if (v == undefined) throw "Missing v";
+    return N - i;
+  }
+  throw "Unknown tag";
+};
+
+// const N = 10007;
+const N = 10;
 
 function solution() {
   let input = readInput();
 
   let stack = _.range(N);
+  console.log(stack);
   for (let step of input) {
     stack = next(stack, step);
     if (stack.length != N) throw "oops";
   }
-  console.log(
-    "n=2019",
-    _.findIndex(stack, n => n == 2019)
-  );
-  console.log(next(_.range(10), { tag: Tag.Cut, v: -4 }));
+  // console.log(
+  //   "n=2019",
+  //   _.findIndex(stack, n => n == 2019)
+  // );
+  console.log(stack);
+  let result = _.range(N).map(i => input.reduce(calc, i));
+  console.log(result);
 }
 
 export default solution;
